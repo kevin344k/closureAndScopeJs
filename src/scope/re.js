823d11a5ebd566b339a8b2bdaead35a7c9e784ef
kevin344k <kevin344k@gmail.com>
2023-04-27 17:09:19 +0000

    strict mode clase 6

---
+++ src/scope/re.js
@@ -0,0 +1,34 @@
+var firstName; //undefined
+firstName='Kevin'
+//console.log(firstName)
+
+var lastName='Isarel' //declarar /asignar
+lastName='Arizaga' //reasignar
+//console.log(lastName)
+
+var secondName='Kevin' //declarando /asignando
+var secondName='Anna' //reasignando
+//console.log(secondName)
+
+//let
+
+let fruit='Apple' //declarar y asignar
+fruit='kiwi' //reasignar
+
+
+
+//let fruit ='Banana'
+ // console.log(fruit)
+
+// const 
+const animal='dog' //declara y asigna
+//const animal='cat'
+//console.log(animal)
+
+const vehicles= []
+vehicles.push('🚗')
+console.log(vehicles)
+
+
+vehicles.pop()
+  console.log(vehicles)
